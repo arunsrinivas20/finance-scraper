da5e4602c625d1ab09338f3c03deb29725eb08f5
Arun Srinivas <asrini19@Aruns-MBP.fios-router.home>
2019-09-07 13:58:47 -0400

    updated routine to determine transaction partners and the reason. Also created handling case for empty reason

--- extension/utils.js
+++ extension/utils.js
@@ -74,19 +74,33 @@ export function parseHTMLArrForTransactionsC1(htmlArr) {
     return res;
 }
 
-function modifyTransactionReason(reason, person, requested) {
+function modifyTransactionReason(reason, person1, person2, charged) {
     let res = '';
 
-    if (reason.indexOf('ppco') >= 0 || reason.indexOf('pep') >= 0) {
-        res = `Pepco Bill - ${person}`;
-    } else if (reason.indexOf('vzon') >= 0) {
-        res = `Verizon Bill - ${person}`;
-    } else {
-        if (requested) {
-            res = `Paid back ${person} for ${reason}`;
+    if (charged) {
+        if (person1 === 'Arun Srinivas') {
+            if (reason.indexOf('ppco') >= 0 || reason.indexOf('pep') >= 0 || reason.indexOf('pepco') >= 0) {
+                res = `Pepco Bill - ${person2}`;
+            } else if (reason.indexOf('vzon') >= 0 || reason.indexOf('verizon') >= 0) {
+                res = `Verizon Bill - ${person2}`;
+            } else {
+                res = `Paid back by ${person2} for ${reason}`;
+            }
         } else {
-            res = `Paid back by ${person} for ${reason}`;
-        }   
+            res = `Paid back ${person1} for ${reason}`;
+        }
+    } else { // Means the keyword was paid
+        if (person1 === 'Arun Srinivas') {
+            res = `Paid back ${person2} for ${reason}`;
+        } else {
+            if (reason.indexOf('ppco') >= 0 || reason.indexOf('pep') >= 0 || reason.indexOf('pepco') >= 0) {
+                res = `Pepco Bill - ${person2}`;
+            } else if (reason.indexOf('vzon') >= 0 || reason.indexOf('verizon') >= 0) {
+                res = `Verizon Bill - ${person2}`;
+            } else {
+                res = `Paid back by ${person2} for ${reason}`;
+            }
+        }
     }
 
     return res;
@@ -105,11 +119,18 @@ export function parseHTMLArrForTransactionsVenmo(htmlArr) {
         match = regexp.exec(str);
         const person1 = match[1] + ' ' + match[2];
         const person2 = match[3] + ' ' + match[4];
-        const transactionPartner = person1 !== 'Arun Srinivas' ? person1 : person2;
+        
+        regexp = /<\/strong><\/a><span><!-- react-text: \d+ --> <!-- \/react-text --><!-- react-text: \d+ -->(\w+)</;
+        match = regexp.exec(str);
+        const charged = match[1] === 'charged'; // if true, then person1 CHARGED person2, else, person1 PAID person2
+
         regexp = /<div class="feed-description__notes__content"><p><!-- react-text: \d+ -->([A-Za-z\d\s\W]*)<!--/
         match = regexp.exec(str);
-        let reason = match[1];
-        reason = modifyTransactionReason(reason.toLowerCase(), transactionPartner, transactionPartner === person1);
+        let reason = '';
+        if (match) {
+            reason = match[1]
+        }
+        reason = modifyTransactionReason(reason.toLowerCase(), person1, person2, charged);
 
         regexp = /<div class="feed-description__amount"><span class="[A-Za-z]*">(-?)\$([\d,]+\.\d+)</
         match = regexp.exec(str);
